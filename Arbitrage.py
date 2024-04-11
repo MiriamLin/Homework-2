6d5fc2e32d7cea3e1756678b7db333daf490f8b3
Miriam Lin <miriamlin@MiriamdeMacBook-Air.local>
2024-04-11 09:15:22 +0800

    second

--- Arbitrage.py
+++ Arbitrage.py
@@ -20,7 +20,42 @@ a = [
     [9, 6, 12, 0, 60],
     [5, 3, 8, 25, 0]
 ]
-b = [1, 0, 2, 4, 3, 2, 1]
+temp = [1, 0, 0, 0, 0, 0, 1]
+temp2 = [[0 for _ in range(5)] for _ in range(5)]
+for i in range(0,5):
+    for j in range(0,5):
+        for k in range(0,5):
+            for l in range(0,5):
+                for m in range(0,5):
+                    if i==1 or j==1 or k==1 or l==1 or m==1:
+                        continue
+                    temp[1]=i
+                    temp[2]=j
+                    temp[3]=k
+                    temp[4]=l
+                    temp[5]=m
+                    bb=1
+                    for u in range(0,5):
+                        for v in range(0,5):
+                            temp2[u][v]=0
+                    for u in range(0,6):
+                        if temp[u]==temp[u+1]:
+                            bb=0
+                        temp2[temp[u]][temp[u+1]]+=1
+                        temp2[temp[u+1]][temp[u]]+=1
+                    for u in range(0,5):
+                        for v in range(0,5):
+                            if temp2[u][v]>1:
+                                bb=0
+                    if bb==0:
+                        continue
+                    now=5.0
+                    for u in range(0,6):
+                        now=997*now*a[temp[u+1]][temp[u]]/(1000*a[temp[u]][temp[u+1]]+997*now)
+                    if now>20.0:
+                        for u in range(0,7):
+                            b[u]=temp[u]
+                        break
 c[0] = 5.0
 for i in range(1,7):
     x = a[b[i-1]][b[i]]
